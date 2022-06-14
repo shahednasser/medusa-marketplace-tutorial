36ba2c56c5710ffe2ddf4ce6f40f2de4deabc211
Shahed Nasser <shahednasser@gmail.com>
2022-06-14 19:31:38 +0300

    resolve error in new medusa extender version

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,7 +3,7 @@ import { Module } from 'medusa-extender';
 import { Product } from './entities/product.entity';
 import ProductRepository from './repositories/product.repository';
 import { ProductService } from './services/product.service';
-import addStoreIdToProduct1645034402086 from './migrations/product.migration';
+import addStoreIdToProduct1645034402086 from './product.migration';
 
 @Module({
     imports: [
