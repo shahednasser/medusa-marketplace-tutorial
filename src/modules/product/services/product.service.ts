cfe2ea40f1ca38e284b43eb5ece4cf2f581ddb14
Shahed Nasser <shahednasser@gmail.com>
2022-06-15 12:38:16 +0300

    add new dependencies for v1.3.1 of medusa

--- src/modules/product/services/product.service.ts
+++ src/modules/product/services/product.service.ts
@@ -20,6 +20,8 @@ type ConstructorParams = {
     imageRepository: any;
     searchService: any;
     userService: UserService;
+    cartRepository: any;
+    priceSelectionStrategy: any;
 }
 
 @Service({ scope: 'SCOPED', override: MedusaProductService })
