c4c96f2e077bb41786982a9b91262a5d3c84abd9
Shahed Nasser <shahednasser@gmail.com>
2022-06-15 14:48:59 +0300

    rename permissions repository file

--- src/modules/permission/permission.module.ts
+++ src/modules/permission/permission.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "medusa-extender";
 import { Permission } from "./permission.entity";
 import { PermissionMigration1655131601491 } from "./1655131601491-permission.migration";
-import { PermissionRepository } from "./permissions.repository";
+import { PermissionRepository } from "./permission.repository";
 
 @Module({
   imports: [
